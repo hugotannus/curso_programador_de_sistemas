5848ae6ac16f8d86ca526a9e95f43c462ac43d75
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-08-27 16:46:18 -0300

    Define cenários de teste para exercício 18 da Lista #4.

---
+++ Exercícios/JavaScript/Lista_4/18_conta-vogais.js
@@ -0,0 +1,10 @@
+function contaVogais(palavra) {
+    let contador = 0;
+
+    return contador; 
+}
+
+console.log(contaVogais("hello") === 2);
+console.log(contaVogais("javascript") === 3);
+console.log(contaVogais("aeiou") === 5);
+console.log(contaVogais("xyz") === 0);
