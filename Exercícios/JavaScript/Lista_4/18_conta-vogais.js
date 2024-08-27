1c3028dbe284606467bb0e5e78cd7559c0351a86
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-08-27 16:57:35 -0300

    Resolução do exercício 18, Lista #4.

--- Exercícios/JavaScript/Lista_4/18_conta-vogais.js
+++ Exercícios/JavaScript/Lista_4/18_conta-vogais.js
@@ -1,10 +1,24 @@
 function contaVogais(palavra) {
     let contador = 0;
 
+    for(let i=0; i < palavra.length; i++) {
+        let char = palavra.charAt(i);
+
+        if(char == "a") contador++;
+        if(char == "e") contador++;
+        if(char == "i") contador++;
+        if(char == "o") contador++;
+        if(char == "u") contador++;
+    }
+
     return contador; 
 }
 
+console.log(contaVogais("hello"));
 console.log(contaVogais("hello") === 2);
+console.log(contaVogais("javascript"));
 console.log(contaVogais("javascript") === 3);
+console.log(contaVogais("aeiou"));
 console.log(contaVogais("aeiou") === 5);
+console.log(contaVogais("xyz"));
 console.log(contaVogais("xyz") === 0);
