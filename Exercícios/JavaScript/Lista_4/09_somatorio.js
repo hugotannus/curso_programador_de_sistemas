9b677e7d517be75aa8f6482fdecdee850c33dc40
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-08-29 14:41:36 -0300

    Ajustando cenários de teste para o exercício 9, lista 4.

--- Exercícios/JavaScript/Lista_4/09_somatorio.js
+++ Exercícios/JavaScript/Lista_4/09_somatorio.js
@@ -2,8 +2,8 @@
 
 const somaLista = require("./modulo");
 
-console.log(somaLista([1, 2, 3, 4, 5]));
-console.log(somaLista([2, 4, 6, 8]));
-console.log(somaLista([1, 3, 5, 7]));
-console.log(somaLista([0]));
-console.log(somaLista([]));
+console.log(somaLista([1, 2, 3, 4, 5]) === 15);
+console.log(somaLista([2, 4, 6, 8]) === 20);
+console.log(somaLista([1, 3, 5, 7]) === 16);
+console.log(somaLista([0]) === 0);
+console.log(somaLista([]) === 0);
