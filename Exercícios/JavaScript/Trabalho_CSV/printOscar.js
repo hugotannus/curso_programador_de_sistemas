cffff8752da74d6b419ff9841c6749c25a567b95
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-08-30 13:54:21 -0300

    Imprime quantidade variável de colunas de um arquivo CSV.

--- Exercícios/JavaScript/Trabalho_CSV/printOscar.js
+++ Exercícios/JavaScript/Trabalho_CSV/printOscar.js
@@ -3,5 +3,7 @@ const fs = require('fs');
 const { printCSV } = require('csv');
 
 const csvOscar = fs.readFileSync('./oscar_best_pictures_from_xls.csv', 'utf-8');
+const columns = ["Filme", "Diretor", "Ano", "Duração", "Gênero"]
 
-printCSV(csvOscar);
+// printCSV(csvOscar);
+printCSV(csvOscar, columns);
